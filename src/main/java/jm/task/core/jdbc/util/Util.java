ab1e7b9f46cb306547754cb95346943c23b748d6
Magomedkhan <magomedkhankad@gmail.com>
2023-08-11 11:36:56 +0300

    ignore

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/data_basa";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "MagomedKhanKadirov1488";
+    public static Connection getConnect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+        } catch (SQLException e) {
+            System.err.println("не удалось загрузить драйвер");
+        }
+        return connection;
+    }
 }
